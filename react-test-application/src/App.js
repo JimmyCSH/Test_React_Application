0de6c0398efd20439ac246ad96198c369eb336de
Jimmy <jimmycshaw@hotmail.co.nz>
2022-03-18 22:55:37 +1300

    Added a Title with a Card

--- react-test-application/src/App.js
+++ react-test-application/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test React Application
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Application
-        </a>
       </header>
+
+      <div class="outer">
+        <div class="card">
+          <h2>Title</h2>
+        </div>
+      </div>
+
     </div>
   );
 }
